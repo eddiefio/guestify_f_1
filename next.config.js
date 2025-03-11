52ec7c16bdabcd93520ba25edae0db4610bc02b9
eddiefio <edo.fiorio.01@gmail.com>
2025-03-11 18:35:26 +0100

    8o commit

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-      domains: ['ndiqnzxplopcbcxzondp.supabase.co'], // Add your Supabase URL domain
+      domains: ['ndiqnzxplopcbcxzondp.supabase.co'], // Add your Supabase domain
     },
+    // Default output directory that Vercel expects
+    distDir: '.next',
   }
   
   module.exports = nextConfig
