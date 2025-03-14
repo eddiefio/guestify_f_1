f5269c5b9c0a3a3de6afd319d342544582342caf
eddiefio <edo.fiorio.01@gmail.com>
2025-03-14 14:31:53 +0100

    15o commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,11 @@
-// pages/_app.js
+// pages/_app.js - Modified to remove unnecessary loading spinner
 import '../styles/global.css';
 import Head from 'next/head';
-import { useState, useEffect } from 'react';
 import { AuthProvider } from '../contexts/AuthContext';
 import { CartProvider } from '../contexts/CartContext';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function MyApp({ Component, pageProps }) {
-  const [isClient, setIsClient] = useState(false);
-
-  // Questo effetto garantisce che l'idratazione sia completata
-  useEffect(() => {
-    setIsClient(true);
-    
-    // Clear potentially corrupted session state on signin page
-    if (typeof window !== 'undefined' && window.location.pathname === '/auth/signin') {
-      const hasAuthIssue = localStorage.getItem('auth_session') && 
-                          !sessionStorage.getItem('auth_verified');
-      
-      if (hasAuthIssue) {
-        console.log('Detected potential corrupted session, clearing...');
-        localStorage.removeItem('auth_session');
-        
-        // Clear any auth-related cookies
-        document.cookie = 'supabase-auth=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        document.cookie = 'supabase-access-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        document.cookie = 'supabase-user=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        document.cookie = 'recent-signin=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-      }
-      
-      // Set flag to avoid checking again
-      sessionStorage.setItem('auth_verified', 'true');
-    }
-    
-    // Safety timeout to ensure isClient gets set to true
-    const timeoutId = setTimeout(() => {
-      if (!isClient) {
-        console.warn('Client hydration timed out, forcing completion');
-        setIsClient(true);
-      }
-    }, 2000); // 2 seconds timeout
-    
-    return () => clearTimeout(timeoutId);
-  }, [isClient]);
-
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
@@ -61,19 +23,11 @@ function MyApp({ Component, pageProps }) {
         referrerPolicy="no-referrer" 
       />
       
-      {/* Even if client-side hydration is not complete, 
-          render a basic loading indicator rather than nothing */}
-      {!isClient ? (
-        <div className="flex justify-center items-center min-h-screen">
-          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500"></div>
-        </div>
-      ) : (
-        <AuthProvider>
-          <CartProvider>
-            {getLayout(<Component {...pageProps} />)}
-          </CartProvider>
-        </AuthProvider>
-      )}
+      <AuthProvider>
+        <CartProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </CartProvider>
+      </AuthProvider>
     </>
   );
 }
